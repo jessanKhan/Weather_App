6341bdb6fdaf7c079c4981f7a5efd1cdf20aceb3
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2019-12-31 16:08:29 +0600

    some fix

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import AppMain from "./app/app.js";
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -16,17 +16,21 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class AppMain extends Component {
   render() {
     return (
-      <AppMain/>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {l,
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
